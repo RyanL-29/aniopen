3647164193d424343e5c4e2942376306cb0435a4
Ryan <facebook0209@gmail.com>
2021-05-23 22:28:55 +0800

    scoll top function

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
 document.write('<script src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"></script>');
 document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/ionicons@2.0.1/css/ionicons.min.css">');
-document.write('<link rel="manifest" href="//cdn.jsdelivr.net/gh/RyanL-29/aniopen@1.4.8/manifest.json">');
+document.write('<link rel="manifest" href="//cdn.jsdelivr.net/gh/RyanL-29/aniopen@1.4.9/manifest.json">');
 document.write('<link rel="apple-touch-icon" href="//cdn.jsdelivr.net/gh/RyanL-29/aniopen/pwa_icon/192x192nt.png">');
 // markdown支持
 document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
 // DPlayer API
-document.write('<script src="//cdn.jsdelivr.net/gh/RyanL-29/aniopen@1.4.8/DPlayer.min.js"></script>');
+document.write('<script src="//cdn.jsdelivr.net/gh/RyanL-29/aniopen@1.4.9/DPlayer.min.js"></script>');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
 
 // 初始化页面，并载入必要资源
@@ -31,8 +31,8 @@ function init() {
   <footer>
     <p style="line-height: 0;">ani.rip</p>
     <div style="line-height: 0;">
-    <p class="footer-ele"><a onclick="contactus()">聯絡我們</a></p><ins></ins>
-    <p class="footer-ele"><a onclick="dmca()">DMCA</a></p>
+    <p class="footer-ele"><a onclick="contactus(); topFunction();">聯絡我們</a></p><ins></ins>
+    <p class="footer-ele"><a onclick="dmca(); topFunction();">DMCA</a></p>
     </div>
     <p class="footer-ele2">本網站只用作交流學習以及試看</p>
     <p class="footer-ele2">如發現網站有任何非法資源 請立即聯絡我們移除</p>
@@ -63,6 +63,11 @@ function timeout() {
     timeout = setTimeout(function () {
         globalsearch()
     }, delay);
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 function globalsearch() {
