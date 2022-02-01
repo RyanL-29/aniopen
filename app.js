55486cec9d05f267793f26a977e605bf88379a51
M-Desktop\Ryan <facebook0209@gmail.com>
2022-02-01 19:49:46 +0800

    fix chromeCast duplicate init

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const version = "1.8.7"
+const version = "1.8.8"
 
 document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
 document.write('<script src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"></script>');
